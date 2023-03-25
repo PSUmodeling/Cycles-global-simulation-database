9abe4749494adfd4f9feda435d6170700775bca6
Yuning Shi <shiyuning@gmail.com>
2023-03-24 23:00:12 -0400

    :fire: Remove blank lines

--- bin/generate_global_lookup.py
+++ bin/generate_global_lookup.py
@@ -271,4 +271,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.crop)
-
--- bin/generate_global_soil.py
+++ bin/generate_global_soil.py
@@ -168,4 +168,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.crop)
-
